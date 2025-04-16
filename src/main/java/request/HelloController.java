0d8b3c8a7ee065de49c5c558e0ffffd2e802ebf7
henrique.silva <rico.henrique9@gmail.com>
2025-04-16 17:53:51 -0300

    metodo delete adicionado classe Hello Controlle

--- src/main/java/request/HelloController.java
+++ src/main/java/request/HelloController.java
@@ -35,6 +35,12 @@ public class HelloController {
         System.out.println("Chegou o update");
         return userRequest;
     }
+
+    @DeleteMapping
+    public UserRequest deleteUser(UserRequest userRequest) {
+        System.out.println("Deletar usuário?");
+        return userRequest;
+    }
 }
 
 
@@ -47,7 +53,7 @@ no get, não recebe nada e retorna um List<UserRequest>
 
 no getById retorna um UserRequest e recebe um Id
 
-no update retorna um UserRequest e recebe um UserRequest e Id
+
 
 no delete temos que ver se não vamos retornar nada ou se vamos retornar uma mensagem (recebemos um id)
 consegue mapear isso mais ou menos?
